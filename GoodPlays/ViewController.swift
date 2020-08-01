e6d050f11758cd19f2906220068d3456ffdb4dcb
Cesa Satria Anwar <Cesa263@gmail.com>
2020-08-02 00:49:45 +0700

    Initial Commit

---
+++ GoodPlays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodPlays
+//
+//  Created by Cesa Anwar on 02/08/20.
+//  Copyright © 2020 Cesa Anwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
